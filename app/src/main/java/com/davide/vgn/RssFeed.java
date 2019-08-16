9ba3f71abbb39cb931fac0519f5e538378bc639a
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-08-16 19:33:11 +0200

    Unescape HTML in title and description. Bugfix for text color.

--- app/src/main/java/com/davide/vgn/RssFeed.java
+++ app/src/main/java/com/davide/vgn/RssFeed.java
@@ -7,37 +7,37 @@ import java.util.Date;
 
 public class RssFeed implements Serializable {
 
-    public String channelTitle;
-    public String title;
-    public String link;
-    public String description;
-    public Date pubDate;
-    public String image;
+	public String channelTitle;
+	public String title;
+	public String link;
+	public String description;
+	public Date pubDate;
+	public String image;
 
-    public RssFeed(String channelTitle, String title, String link, String description, Date pubDate, String image) {
-        this.channelTitle = channelTitle;
-        this.title = title;
-        this.pubDate = pubDate;
-        this.link = link;
-        this.description = description;
-        this.image = image;
-    }
+	public RssFeed(String channelTitle, String title, String link, String description, Date pubDate, String image) {
+		this.channelTitle = channelTitle;
+		this.title = StringUtils.unescapeHtml3(title);
+		this.pubDate = pubDate;
+		this.link = link;
+		this.description = StringUtils.unescapeHtml3(description);
+		this.image = image;
+	}
 
-    @Override
+	@Override
 	public boolean equals(Object other) {
-    	if(other != null && other.getClass() == RssFeed.class) {
-    		RssFeed o = (RssFeed)other;
-    		try {
-			    return channelTitle.equals(o.channelTitle) &&
-					    title.equals(o.title) &&
-					    link.equals(o.link) &&
-					    description.equals(o.description) &&
-					    pubDate.equals(o.pubDate) &&
-					    (image == null && o.image == null || (image != null && image.equals(o.image)));
-		    } catch(Exception e) {
-    			Log.e(MainActivity.TAG, "RssFeed.equals error: " + e.toString());
-		    }
-	    }
-	    return false;
-    }
+		if (other != null && other.getClass() == RssFeed.class) {
+			RssFeed o = (RssFeed) other;
+			try {
+				return channelTitle.equals(o.channelTitle) &&
+						title.equals(o.title) &&
+						link.equals(o.link) &&
+						description.equals(o.description) &&
+						pubDate.equals(o.pubDate) &&
+						(image == null && o.image == null || (image != null && image.equals(o.image)));
+			} catch (Exception e) {
+				Log.e(MainActivity.TAG, "RssFeed.equals error: " + e.toString());
+			}
+		}
+		return false;
+	}
 }
